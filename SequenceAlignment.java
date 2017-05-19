0b067c1eac95b3c4a2e905e988adf5b43a8d368f
SleekPanther <macrobyte2@gmail.com>
2017-05-18 20:42:27 -0400

    memo table base cases

--- SequenceAlignment.java
+++ SequenceAlignment.java
@@ -11,7 +11,40 @@ public class SequenceAlignment {
 	String[] vowelsLowerCase = {"a", "e", "i", "o", "u"};
 	String[] conconantsLowerCase = {"b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"};
 
+	int[][] memoTable;
+	int[][][] predecessorIndexes;
+
+
 	public SequenceAlignment(){
+	}
+
+	public void calcOptimalAlignment(String seq1, String seq2){
+		seq1 = seq1.trim();
+		seq2 = seq2.trim();
+
+		memoTable = new int[seq1.length() +1][seq2.length() +1];
+
+		for(int i=0; i<=seq1.length(); i++){
+			memoTable[i][0] = i * GAP_PENALTY;		// base case: j = 0
+		}
+		for(int j=0; j<=seq2.length(); j++){
+			memoTable[0][j] = j * GAP_PENALTY;		// base case: j = 0
+		}
+
+		printTable(memoTable);
+	}
+
+	private void printTable(int[][] table){
+		for(int[] row : table){
+			System.out.print(row[0]);
+			for(int j=1; j<row.length; j++){
+				System.out.print("\t"+row[j]);
+			}
+			System.out.println();
+		}
+	}
+
+	private void findAlignment(){
 
 	}
 
@@ -35,11 +68,14 @@ public class SequenceAlignment {
 	public static void main(String[] args) {
 		SequenceAlignment sequenceAligner = new SequenceAlignment();
 
-
 		System.out.println(sequenceAligner.mismatchPenalty(" a  ", " A            "));
 		System.out.println(sequenceAligner.mismatchPenalty(" a  ", " I            "));
 		System.out.println(sequenceAligner.mismatchPenalty("b", "x"));
 		System.out.println(sequenceAligner.mismatchPenalty("a", "z"));
+
+		String testString1 = "abcd";
+		String testString2 = "abc";
+		sequenceAligner.calcOptimalAlignment(testString1, testString2);
 	}
 
 }
