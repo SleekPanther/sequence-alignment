b10197aae61722989617533e154bd76eac7d2114
SleekPanther <macrobyte2@gmail.com>
2017-05-20 08:53:07 -0400

    new findAlignment WORKS!

--- SequenceAlignment.java
+++ SequenceAlignment.java
@@ -79,8 +79,9 @@ public class SequenceAlignment {
 		System.out.println("\nPredecessor table (where the values came from)");
 		printTable3D(predecessorIndexes);
 
-		System.out.println("\nMinimum penalty for aligning "+seq1 +" & "+seq2);
-		findAlignment(seq1, seq2, predecessorIndexes);
+		System.out.println("\n" + memoTable[seq1.length()-1][seq1.length()-1] + "\t is the Minimum penalty for aligning "+seq1 +" & "+seq2);
+		findAlignment(seq1, seq2);
+		// findAlignment(seq1, seq2, predecessorIndexes);
 	}
 
 	private void printTable(int[][] table){
@@ -100,6 +101,50 @@ public class SequenceAlignment {
 			}
 			System.out.println();		//Ends up printing a trailing newline
 		}
+	}
+
+	private void findAlignment(String seq1, String seq2){
+		String seq1Aligned = "";
+		String seq2Aligned = "";
+
+		int i = seq1.length()-1;
+		int j = seq2.length()-1;
+
+		//Retrace the memoTable calculations. Stops when reaches the start of 1 sequence (so additional gaps may still need to be added to the other)
+		while(i>0 && j>0){
+			if(memoTable[i][j] - mismatchPenalty(seq1.charAt(i)+"", seq2.charAt(j)+"") == memoTable[i-1][j-1]){
+				seq1Aligned = seq1.charAt(i) + seq1Aligned;
+				seq2Aligned = seq2.charAt(j) + seq2Aligned;
+				i=i-1;
+				j=j-1;
+				System.out.println("i-1 : j-1 \t" +seq1Aligned+"\t\t|"+seq2Aligned);
+			}
+			else if(memoTable[i][j] - GAP_PENALTY == memoTable[i][j-1]){
+				seq2Aligned = seq2.charAt(j) + seq2Aligned;
+				seq1Aligned = "_" + seq1Aligned;
+				j=j-1;
+				System.out.println("j-1 (2 w Gap) \t" +seq1Aligned+"\t\t|"+seq2Aligned);
+			}
+			else if(memoTable[i][j] - GAP_PENALTY == memoTable[i-1][j]){
+				seq1Aligned = seq1.charAt(i) + seq1Aligned;
+				seq2Aligned = "_" + seq2Aligned;
+				i=i-1;
+				System.out.println("i-1 (1 with gap) \t"  +seq1Aligned+"\t\t|"+seq2Aligned);
+			}
+		}
+		//Now i==0 or j==0 or both. Finish by adding any additional leading gaps to the start of the sequence whoes pointer ISN'T == 0
+		while(j>0){
+			seq2Aligned = seq2.charAt(j) + seq2Aligned;
+			seq1Aligned = "_" + seq1Aligned;
+			j=j-1;
+		}
+		while(i>0){
+			seq1Aligned = seq1.charAt(i) + seq1Aligned;
+			seq2Aligned = "_" + seq2Aligned;
+			i=i-1;
+		}
+
+		System.out.println("\nOptimal Alignment:\n" +seq1Aligned+"\n"+seq2Aligned);
 	}
 
 	//Retrace steps the find the actual alignment. Only call this after calcOptimalAlignment() has been called
@@ -221,13 +266,15 @@ public class SequenceAlignment {
 	public static void main(String[] args) {
 		SequenceAlignment sequenceAligner = new SequenceAlignment();
 
-		System.out.println(sequenceAligner.mismatchPenalty(" a  ", " A            "));
-		System.out.println(sequenceAligner.mismatchPenalty(" a  ", " I            "));
-		System.out.println(sequenceAligner.mismatchPenalty("b", "x"));
-		System.out.println(sequenceAligner.mismatchPenalty("a", "z"));
+		// System.out.println(sequenceAligner.mismatchPenalty(" a  ", " A            "));
+		// System.out.println(sequenceAligner.mismatchPenalty(" a  ", " I            "));
+		// System.out.println(sequenceAligner.mismatchPenalty("b", "x"));
+		// System.out.println(sequenceAligner.mismatchPenalty("a", "z"));
 
 		String testString1 = "mean";
 		String testString2 = "name";
+		// String testString1 = "acbd";
+		// String testString2 = "zcbd";
 		sequenceAligner.calcOptimalAlignment(testString1, testString2);
 	}
 
